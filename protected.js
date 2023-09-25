2beeb727e067e0353303cf8ead46ba6028a25d50
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-09-25 19:56:15 +0530

    Added homepage which contains registration / login forms.

---
+++ protected.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+module.exports = {
+    mongoDB_atlas_url: process.env.mongoDB_atlas_url
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+// Importing user defined functions and files
+const protected = require("./protected");
+
+const app = express();
+
+// Setting up MongoDB
+mongoose.set("strictQuery",false);
+mongoose.connect(protected.mongoDB_atlas_url);
+
+// Middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.set("view engine","ejs");
+
+// Adding routes
+app.use("/", require("./Routes/index"));
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000. Go to http://localhost:3000");
+});
