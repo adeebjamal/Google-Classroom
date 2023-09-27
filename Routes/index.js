4e960a327c9730fd5ea8df7fe741f287abfdeabb
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-09-27 21:40:00 +0530

    Users don't need to login each time they visit the website.

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,44 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
-router.get("/", (req,res) => {
-    res.status(200).render("homepage", {
-        message: ""
-    });
+// Importing user defined files and functions.
+const protected = require("../protected");
+const STUDENT = require("../Models/student");
+const FACULTY = require("../Models/faculty");
+const CLASSROOM = require("../Models/classroom");
+
+router.get("/", async(req,res) => {
+    try {
+        if(req.cookies.JWT_token_student) {
+            const decodedJWT = jwt.verify(req.cookies.JWT_token_student, protected.SECRET_KEY);
+            const foundStudent = await STUDENT.findOne({_id: decodedJWT.ID});
+            const foundClassrooms = await CLASSROOM.find({_id: {$in: foundStudent.classrooms}});
+            return res.status(200).render("student-dashboard", {
+                name: foundStudent.name,
+                classrooms: foundClassrooms,
+                message: ""
+            });
+        }
+        else if(req.cookies.JWT_token_faculty) {
+            const decodedJWT = jwt.verify(req.cookies.JWT_token_faculty, protected.SECRET_KEY);
+            const foundFaculty = await FACULTY.findOne({_id: decodedJWT.ID});
+            const facultyClassrooms = await CLASSROOM.find({facultyID: decodedJWT.ID});
+            return res.status(200).render("faculty-dashboard", {
+                name: foundFaculty.name,
+                message: "",
+                classrooms: facultyClassrooms
+            });
+        }
+        return res.status(200).render("homepage", {
+            message: ""
+        });
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).render("homepage", {
+            message: "Something went wrong, try again."
+        });
+    }
 });
 
 module.exports = router;
